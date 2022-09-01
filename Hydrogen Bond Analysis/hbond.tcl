0a090ce15f569206a00cbaa8dc81d598d739031e
Biomolecular Simulations Group <91871986+bslgroup@users.noreply.github.com>
2022-09-01 10:12:53 -0500

    Update hbond.tcl

--- Hydrogen Bond Analysis/hbond.tcl
+++ Hydrogen Bond Analysis/hbond.tcl
@@ -3,9 +3,9 @@ package require hbonds
 mol new input.psf
 mol addfile input.dcd type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
 set all [atomselect $i "all"]
-set TM4 [atomselect $i "protein and resid 516 and sidechain"]
-set TM5 [atomselect $i "protein and resid 429 and sidechain"]
+set Sel1 [atomselect $i "protein and resid $resid and sidechain"]
+set Sel2 [atomselect $i "protein and resid $resid and sidechain"]
 
-hbonds -sel1 $TM4 -DA both -sel2 $TM5 -DA both -dist 4.0 -ang 30 -polar yes -type unique -writefile yes -outdir outputname-0 -log outputname.log -outfile outputname.hbonds.dat -detailout outputname.stdout
+hbonds -sel1 $Sel1 -DA both -sel2 $Sel2 -DA both -dist 4.0 -ang 30 -polar yes -type unique -writefile yes -outdir outputname-0 -log outputname.log -outfile outputname.hbonds.dat -detailout outputname.stdout
 
 quit
